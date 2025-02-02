5df6e7ee3fbb2f7e58a3e36de8c30f76e4f16e85
TomasPino5 <tomaspino48@gmail.com>
2025-02-02 13:22:44 -0300

    comitx

--- public/views/home/index.js
+++ public/views/home/index.js
@@ -6,6 +6,39 @@ function getRandomObjects(value) {
     return value
 }
 
+function renderedCards(value, index) {
+    document.getElementById('videoCardsContainer').innerHTML = '';
+
+    let slicedValue;
+    if (index == 1) {
+        slicedValue = value.slice(0, 25);
+    } else if (index == 2) {
+        slicedValue = value.slice(25, 50); 
+    } else if (index == 3) {
+        slicedValue = value.slice(50, 75); 
+    } else if (index == 4) {
+        slicedValue = value.slice(75, 100); 
+    } else if (index == 5) {
+        slicedValue = value.slice(100, 125); 
+    } else if (index == 100) {
+        slicedValue = value.slice(0, 125); 
+    }
+
+    slicedValue.forEach(element => {
+        document.getElementById('videoCardsContainer').innerHTML += `
+            <a class="cardVideos" style="width: 10rem; text-decoration: none" href="${element.link}" target="_blank">
+                <img src="${element.thumbnail}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${element.title}</h5>
+                    <p class="card-text first">${element.language}</p>
+                    <p class="card-text">${element.channel}</p>
+                    <p class="card-text">${element.views}M Vistas - ${element.duration}${element.duration > 1 ? ' Horas' : ' Hora'}</p>
+                    ${element.languageIcon.map(icon => `<img class="img-language" src="${icon}" alt="">`).join(' ')}
+                </div>
+            </a>`;
+    });
+}
+
 let loadCourses = () => {
     fetch('../data/videos.json')
     .then(response => response.json())
@@ -19,7 +52,7 @@ let loadCourses = () => {
             
         let index = 1
         let actualIndex = 1
-            
+         
         document.getElementById('start').classList.add('disabled')
             
         const updateActive = (newIndex) => {
@@ -83,53 +116,53 @@ let loadCourses = () => {
             }
         }
 
-        let renderVideos = (videos) => {
-            videos.forEach(element => {
-                document.getElementById('videoCardsContainer').innerHTML += `
-                <a class="cardVideos" style="width: 10rem; text-decoration: none" href="${element.link}" target="_blank">
-                    <img src="${element.thumbnail}" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">${element.title}</h5>
-                        <p class="card-text first">${element.language}</p>
-                        <p class="card-text">${element.channel}</p>
-                        <p class="card-text">${element.views}M Vistas - ${element.duration}${element.duration > 1 ? ' Horas' : ' Hora'}</p>
-                        ${element.languageIcon.map(icon => `<img class="img-language" src="${icon}" alt="">`).join(' ')}
-                    </div>
-                </a>`
+        //search
+        //crear una nueva ruta para el search, en caso de que el input este vacio que vuelva a la ruta inicio
+        document.getElementById('searchForm').addEventListener('submit', (event) => {
+            event.preventDefault()
+
+            let searchTerm = document.getElementById('inputSearch').value.trim().toLowerCase();
+                
+            let filteredVideos = responseVideos.filter((video) => {
+                if (video.title) {
+                    return video.title.trim().toLowerCase().includes(searchTerm);
+                }
+                    return false
             });
-        }
+            document.getElementById('nav-pagination').classList.add('disabled0');
+    
+            if (filteredVideos.length > 0) {        
+                renderedCards(filteredVideos, 100)
+            } else {
+                document.getElementById('videoCardsContainer').innerHTML = '<h1>Lo sentimos, no se ha encontrado ningun video</h1>'
+            }
+        });
         
         let handlePagination = (actualIndex) => {
             switch (actualIndex) {
                 case 1: 
-                document.getElementById('videoCardsContainer').innerHTML = ''
-                renderVideos(responseVideos.slice(0, 25))
+                renderedCards(responseVideos, 1)
                     break;
                 case 2: 
-                document.getElementById('videoCardsContainer').innerHTML = ''
-                renderVideos(responseVideos.slice(25, 50))
+                renderedCards(responseVideos, 2)
                     break;
                 case 3: 
-                document.getElementById('videoCardsContainer').innerHTML = ''
-                renderVideos(responseVideos.slice(50, 75))
+                renderedCards(responseVideos, 3)
                     break;
                 case 4: 
-                document.getElementById('videoCardsContainer').innerHTML = ''
-                renderVideos(responseVideos.slice(75, 100))
+                renderedCards(responseVideos, 4)
                     break;
                 case 5: 
-                document.getElementById('videoCardsContainer').innerHTML = ''
-                renderVideos(responseVideos.slice(100, 126))
+                renderedCards(responseVideos, 5)
                     break;
                 default: console.log("Lo siento, ocurrio un error al reenderizar los videos")
             } 
         }
-
         handlePagination(actualIndex)
     })
 }
 loadCourses()
-
+  
 
 let carrousel = () => {
     let imgOne = document.getElementById('img1')
--- public/views/js/js.js
+++ public/views/js/js.js
@@ -7,23 +7,6 @@ function getRandomObjects(value) {
     return filterValue
 }
 
-function renderedCards(value) {
-    document.getElementById('videoCardsContainer').innerHTML = ''
-    value.forEach(element => {
-        document.getElementById('videoCardsContainer').innerHTML += `
-        <a class="cardVideos" style="width: 10rem; text-decoration: none" href="${element.link}" target="_blank">
-            <img src="${element.thumbnail}" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">${element.title}</h5>
-                <p class="card-text first">${element.language}</p>
-                <p class="card-text">${element.channel}</p>
-                <p class="card-text">${element.views}M Vistas - ${element.duration}${element.duration > 1 ? ' Horas' : ' Hora'}</p>
-                ${element.languageIcon.map(icon => `<img class="img-language" src="${icon}" alt="">`).join(' ')}
-            </div>
-        </a>`
-    });
-}
-
 function renderedCards(value, index) {
     document.getElementById('videoCardsContainer').innerHTML = '';
 
